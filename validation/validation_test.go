273371b45d5c3004d762ff5d08b96256d2afbc4c
Tim Burrington <tgburrin@gmail.com>
2017-01-25 16:36:44 -0500

    Adding the body of the test for validating required fields.

--- validation/validation_test.go
+++ validation/validation_test.go
@@ -2,7 +2,16 @@ package validation
 
 import (
     "testing"
+    "fmt"
 )
 
 func TestValidateRequiredFields (t *testing.T) {
+    a := make(map[string]interface{})
+    setKey(a, "hello", "world")
+
+    b := []string{"hello"}
+    if err := ValidateRequiredFields(a, b); err != nil {
+        fmt.Println("Validation of required fields failed")
+        t.Fail()
+    }
 }
